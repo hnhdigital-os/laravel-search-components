fc0c7445ed78003fdf098c816c6ff08561bda1bd
Rocco Howard <rocco@hnh.digital>
2022-09-19 00:48:12 +0000

    Improved static analysis

--- src/Search.php
+++ src/Search.php
@@ -11,15 +11,16 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 /**
- * @method static name(string $name)
- * @method static class(string $class)
- * @method static route(...$arguments)
- * @method static request(...$arguments)
- * @method static columns(array $config)
- * @method static searchInfo(array|bool $config)
- * @method static searchInput(array|bool $config)
- * @method static searchEmpty(array|bool $config)
- * @method static tableClass(string $class)
+ * @method name(string $name)
+ * @method class(string $class)
+ * @method route(...$arguments)
+ * @method request(...$arguments)
+ * @method columns(array $config)
+ * @method noEmptyCheck(...$arguments)
+ * @method searchInfo(array|bool $config)
+ * @method searchInput(array|bool $config)
+ * @method searchEmpty(array|bool $config)
+ * @method tableClass(string $class)
  */
 class Search
 {
